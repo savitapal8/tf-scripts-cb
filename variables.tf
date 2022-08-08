a1ada487dd50820693236131ed27870bcd7f4e8c
Savita Pal <savita.pal08@gmail.com>
2022-08-08 17:28:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,6 +27,19 @@ variable "key_IAM_users"{
 variable "key_iam_role"{
     type = string
 }
+variable "skip_initial_version_creation"{
+    type = bool
+    description = "skip_initial_version_creation"
+}
+variable "import_only"{
+    type = bool
+    description = "import_only"
+}
+variable "rotation_period"{
+    type = string
+    description = "key rotation period in seconds"
+    default = "100000s"
+}
 
 #worker pool variables
 variable "worker_pool_name"{
