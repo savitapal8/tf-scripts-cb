9bfbcee4dea6417ba39345586f40c8e7ae42c55a
Savita Pal <savita.pal08@gmail.com>
2022-08-08 17:30:24 +0530

    Update variables.tf

--- modules/gcs_bucket/variables.tf
+++ modules/gcs_bucket/variables.tf
@@ -23,6 +23,19 @@ variable "key_IAM_users"{
 variable "key_iam_role"{
     type = string
 }
+variable "skip_initial_version_creation"{
+    type = bool
+    description = "skip_initial_version_creation"
+}
+variable "import_only"{
+    type = bool
+    description = "import_only"
+}
+variable "rotation_period"{
+    type = string
+    description = "key rotation period in seconds"
+    default = "100000s"
+}
 variable "labels"{
     type = map
 }
